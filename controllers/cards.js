55e7e0dc80ea8fa616f5f96fd027454db403671c
DimkaYmka <mungalov00@icloud.com>
2023-06-28 17:32:36 +0300

    erros card update

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,14 +2,22 @@ const cardSchema = require('../models/card');
 
 module.exports.getCards = (req, res) => {
   cardSchema.find({})
-    .then(cards => res.status(201).send(cards))
+    .then(cards => res.status(200).send(cards))
     .catch(err => res.status(500).send({ message: err.message }));
 };
 
 module.exports.deleteCard = (req, res) => {
   cardSchema.findByIdAndRemove(req.params.cardId)
-    .then(card => res.status(201).send(card))
-    .catch(err => res.status(500).send({ message: err.message }));
+    .orFail()
+    .then(card => res.status(200).send(card))
+    .catch(err => {
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(404).send({ message: 'Карточка с данным id не существует.' });
+      }
+      else {
+        res.status(500).send({ message: err.message });
+      }
+    })
 };
 
 module.exports.createCard = (req, res) => {
@@ -17,17 +25,46 @@ module.exports.createCard = (req, res) => {
 
   cardSchema.create({ name, link, owner: req.user._id })
     .then(card => res.status(201).send(card))
-    .catch(err => res.status(500).send({ message: err.message }));
+    .catch(err => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные.' });
+      }
+      else {
+        res.status(500).send({ message: err.message });
+      }
+    });
 };
 
 module.exports.addLikeCard = (req, res) => {
   cardSchema.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
+    .orFail()
     .then(card => res.status(201).send(card))
-    .catch(err => res.status(500).send({ message: err.message }));
+    .catch(err => {
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(404).send({ message: 'Данного id не существует.' });
+      }
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Данные для лайка некорректные.' });
+      }
+      else {
+        res.status(500).send({ message: err.message });
+      }
+    });
 };
 
 module.exports.deleteLikeCard = (req, res) => {
   cardSchema.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
+    .orFail()
     .then(card => res.status(201).send(card))
-    .catch(err => res.status(500).send({ message: err.message }));
+    .catch(err => {
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(404).send({ message: 'Данного id не существует.' });
+      }
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Данные для лайка некорректные.' });
+      }
+      else {
+        res.status(500).send({ message: err.message });
+      }
+    });
 };
