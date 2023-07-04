7483edd8d2f5d249f757e766564c6f1732c25c5b
DimkaYmka <mungalov00@icloud.com>
2023-07-04 17:52:58 +0300

    try9

--- middlewares/celebrate.js
+++ middlewares/celebrate.js
@@ -2,7 +2,7 @@ const { celebrate, Joi } = require('celebrate');
 
 // const urlPattern = /(https?:\/\/)(w{3}\.)?\w+[-.~:/?#[\]@!$&'()*+,;=]*#?/;
 const urlPattern = /^https?:\/\/(?:w{3}\.)?(?:[a-z0-9]+[a-z0-9-]*\.)+[a-z]{2,}(?::[0-9]+)?(?:\/\S*)?#?$/i;
-// const urlPattern = /^https?:\/\/(?:www\.)?[a-z\d-]+(?:\.[a-z\d-]+)*[a-z]{2,}(?::\d+)?(?:\/\S*)?#?$/i;
+
 const validationCreateUser = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
@@ -23,7 +23,7 @@ const validationLogin = celebrate({
 
 const validationUserId = celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().required().length(24).hex(),
+    id: Joi.string().required().length(24).hex(),
   }),
 });
 
@@ -49,7 +49,7 @@ const validationCreateCard = celebrate({
 
 const validationCheckCard = celebrate({
   params: Joi.object().keys({
-    id: Joi.string().required().length(24).hex(),
+    cardId: Joi.string().required().length(24).hex(),
   }),
 });
 
